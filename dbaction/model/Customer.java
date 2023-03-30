ec9aa26bb0fbbe1503af3569190f0b19aeca819d
kio445566 <87538436+kio445566@users.noreply.github.com>
2023-03-30 13:12:32 +0800

    change variable name

--- dbaction/model/Customer.java
+++ dbaction/model/Customer.java
@@ -3,21 +3,21 @@ package dbaction.model;
 import java.sql.*;
 
 public class Customer {
-    private String CID;
-    private String Cname;
+    private String UID;
+    private String Name;
     private String Address;
 
-    public Customer(String CID, String Cname, String Address){
-        this.CID = CID;
-        this.Cname = Cname;
+    public Customer(String UID, String Name, String Address){
+        this.UID = UID;
+        this.Name = Name;
         this.Address = Address;
     }
 
     public void insert(Connection conn) throws SQLException{
         // insert to customer
         PreparedStatement pstmt = conn.prepareStatement("INSERT INTO customer values(?,?,?)");
-        pstmt.setString(1, CID);
-        pstmt.setString(2, Cname);
+        pstmt.setString(1, UID);
+        pstmt.setString(2, Name);
         pstmt.setString(3, Address);
         pstmt.executeUpdate();
         pstmt.close();
