bc12baecf6aaef9b922c8792dfadb4a2c999f6f0
jeff4121336 <jeffyau36@gmail.com>
2023-04-02 11:58:40 +0800

    update db time counter

--- dbaction/dbtime.java
+++ dbaction/dbtime.java
@@ -1,7 +1,10 @@
 package dbaction;
 
+import java.sql.Time;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+
+import dbaction.model.Order;
 
 public class dbtime {
     public static String _dbtime() {
@@ -9,4 +12,22 @@ public class dbtime {
         LocalDateTime now = LocalDateTime.now();
         return d.format(now);
     }
+
+    public int timecount(String OrderTime) { /* Accurate to one day */
+        int diff = -1;
+        String TimeInDigit = OrderTime.replaceAll("[^0-9]", " ");
+        String[] TimeInSlot = TimeInDigit.split(" ");
+        String CurrentTime = _dbtime();
+        String CurrentInDigit = CurrentTime.replaceAll("[^0-9]", " ");
+        String[] CurrentInSlot = CurrentInDigit.split(" ");
+        for (int i = 0; i < TimeInSlot.length - 3; i++) {
+            System.out.println(TimeInSlot[i] +  ", "  + CurrentInSlot[i]);
+            if (TimeInSlot[i] != CurrentInSlot[i]) 
+                return 86401; /* Exceed more than one day */
+        }
+        diff = Integer.parseInt(CurrentInSlot[3]) * 3600  + Integer.parseInt(CurrentInSlot[4]) * 60 + Integer.parseInt(CurrentInSlot[5]) -
+        (Integer.parseInt(TimeInSlot[3]) * 3600  + Integer.parseInt(TimeInSlot[4]) * 60 + Integer.parseInt(TimeInSlot[5]));
+
+        return diff;
+    } 
 }
